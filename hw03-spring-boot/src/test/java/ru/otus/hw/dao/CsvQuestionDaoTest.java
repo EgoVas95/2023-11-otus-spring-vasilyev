cb1b6c82bba0a37e516f281d7caf155a6b0b055f
Egor Vasilyev <egor.vasiliev@globalports.com>
2023-12-27 13:52:51 +0500

    homework 3. Отключил CommandLineRunner через профили

--- hw03-spring-boot/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
+++ hw03-spring-boot/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
 import ru.otus.hw.config.AppProperties;
 import ru.otus.hw.exceptions.QuestionReadException;
 
@@ -14,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
+@ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
 class CsvQuestionDaoTest {
 
