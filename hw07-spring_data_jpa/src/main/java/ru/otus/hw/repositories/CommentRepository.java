1bb6f7a81a8ece006678bbaac1a12c15aee885e3
Egor Vasilyev <egor.vasiliev@globalports.com>
2024-01-23 13:41:05 +0500

    homework 7. add @EntityGraph

--- hw07-spring_data_jpa/src/main/java/ru/otus/hw/repositories/CommentRepository.java
+++ hw07-spring_data_jpa/src/main/java/ru/otus/hw/repositories/CommentRepository.java
@@ -1,10 +1,17 @@
 package ru.otus.hw.repositories;
 
+import jakarta.annotation.Nonnull;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.otus.hw.models.Comment;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Nonnull
+    @EntityGraph("comment-entity-graph")
+    List<Comment> findAll();
+
     List<Comment> findAllByBookId(long bookID);
 }
