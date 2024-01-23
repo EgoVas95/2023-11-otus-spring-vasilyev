ada742060b376eb811b2ab5b96c865f87c86c101
Egor Vasilyev <egor.vasiliev@globalports.com>
2024-01-23 13:41:10 +0500

    homework 7. add @EntityGraph

--- hw07-spring_data_jpa/src/main/java/ru/otus/hw/repositories/BookRepository.java
+++ hw07-spring_data_jpa/src/main/java/ru/otus/hw/repositories/BookRepository.java
@@ -1,5 +1,6 @@
 package ru.otus.hw.repositories;
 
+import jakarta.annotation.Nonnull;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.otus.hw.models.Book;
@@ -9,6 +10,7 @@ import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
+    @Nonnull
     @EntityGraph("book-entity-graph")
     List<Book> findAll();
 }
