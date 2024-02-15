0e4e06267e5937bb19442fdeca0f38daaa2a290f
Egor Vasilyev <egor.vasiliev@globalports.com>
2024-02-15 17:32:21 +0500

    homework 9

--- hw09-mvc/src/main/java/ru/otus/hw/controller/BookController.java
+++ hw09-mvc/src/main/java/ru/otus/hw/controller/BookController.java
@@ -10,12 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ru.otus.hw.dto.BookCreateDto;
 import ru.otus.hw.dto.BookUpdateDto;
 import ru.otus.hw.dto.BookDto;
-import ru.otus.hw.dto.AuthorDto;
-import ru.otus.hw.dto.GenreDto;
 import ru.otus.hw.services.AuthorServiceImpl;
 import ru.otus.hw.services.BookServiceImpl;
 import ru.otus.hw.services.GenreServiceImpl;
