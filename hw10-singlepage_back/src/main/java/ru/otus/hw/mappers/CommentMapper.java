cdf1d520cc89997d45bb1bd72359cb955ba49261
Egor Vasilyev <egor.vasiliev@globalports.com>
2024-02-26 18:34:46 +0500

    homework 10. add ui

--- hw10-singlepage_back/src/main/java/ru/otus/hw/mappers/CommentMapper.java
+++ hw10-singlepage_back/src/main/java/ru/otus/hw/mappers/CommentMapper.java
@@ -2,7 +2,6 @@ package ru.otus.hw.mappers;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
-import ru.otus.hw.dto.BookDto;
 import ru.otus.hw.dto.CommentCreateDto;
 import ru.otus.hw.dto.CommentDto;
 import ru.otus.hw.dto.CommentUpdateDto;
