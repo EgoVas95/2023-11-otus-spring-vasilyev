af743a29c5c2847599a7de7d8e534f5d90e7199d
Egor Vasilyev <vasilev.e@delotech.ru>
2024-05-03 15:41:48 +0500

    hw18

---
+++ hw18-hystrix/src/main/java/ru/otus/hw/Application.java
@@ -0,0 +1,19 @@
+package ru.otus.hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		System.out.println("http://localhost:8080/api/books - список книг");
+		System.out.println("http://localhost:8080/api/books?id=1 - редактирование книги");
+		System.out.println();
+		System.out.println("http://localhost:8080/api/authors - список авторов");
+		System.out.println("http://localhost:8080/api/genres - список жанров");
+		System.out.println("http://localhost:8080/api/books/1/comments - список комментариев для книги");
+	}
+
+}
